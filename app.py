3381a456204243cb217bc09de4ac1f4751a5dbb7
Kumarion <121711454+Kumarion@users.noreply.github.com>
2024-12-22 01:10:40 -0500

    Result

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from rembg import remove
 from PIL import Image
 import requests
 import base64
+import io
 from utils.authenticate import Authenticate
 
 app = Flask(__name__)
@@ -63,35 +64,43 @@ def remove_bg():
         input_type = "url"
 
     # Check if an image was provided either as a file or base64 string
-    if image_data is None:
-        return jsonify({"error": "No image provided"}), 400
+    if image_data is None or type(image_data) == str and image_data == "":
+        return jsonify({"error": "No image data provided"}), 400
     
     # Check the file extension and MIME type
     if input_type == "file":
         if not allowed_file(image_data.filename, image_data.mimetype):
-            return jsonify({"error": "Format was not supported"}), 400
+            return jsonify({"error": "Image Format is not supported"}), 400
         
     # Open the image with PIL
     input_image = get_image_data(image_data, input_type)
         
     # Check the file size
-    file_size = check_file_size(input_image, input_type)
+    file_size = check_file_size(input_image)
 
     # If the file size exceeds the maximum limit, return an error
     if file_size > MAX_FILE_SIZE:
-        return jsonify({"error": f"The file exceeds the maximum size of {MAX_FILE_SIZE / (1024 * 1024)} MB"}), 400  
+        return jsonify({"error": f"The file data exceeds the maximum size of {MAX_FILE_SIZE / (1024 * 1024)} MB"}), 400  
     
     # Remove the background from the image
     output_image = remove(input_image)
+
+    # Create BytesIO object to handle the image data
+    img_io = io.BytesIO()
+    img_io.write(output_image)
+    img_io.seek(0)
+
+    # Return the image directly
+    return send_file(img_io, mimetype='image/png')
     
-    return {
-        "headers": {
-            "Content-Type": "image/png",
-        },
-        "statusCode": 200,
-        "body": base64.b64encode(output_image).decode("utf-8"),
-        "isBase64Encoded": True
-    }
+    # return {
+    #     "headers": {
+    #         "Content-Type": "image/png",
+    #     },
+    #     "statusCode": 200,
+    #     "body": base64.b64encode(output_image).decode("utf-8"),
+    #     "isBase64Encoded": True
+    # }
 
 if __name__ == "__main__":
     app.run(debug=True)
